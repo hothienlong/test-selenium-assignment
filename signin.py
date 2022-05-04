9c3be782f9df90841f7bb71bf1231b8c2700e544
Dang Nguyen <nguyendangit04@gmail.com>
2022-05-04 19:02:18 +0700

    feat: Init Sign In

--- signin.py
+++ signin.py
@@ -6,40 +6,23 @@ from selenium.webdriver.common.by import By
 
 util = Util()
 
-class SignInPage:
-    def signIn(self, email, password):
-        util.go_to_main_page()
-        time.sleep(1)
-        menuBtnEleValue = "//button[@type='button']"
-        util.click_element(By.XPATH, menuBtnEleValue)
-        
-        util.driver.implicitly_wait(1)
-        
-        goToSignInEleValue = '#__next > div.root > header > div > div > div.jsx-3599443646.col-right.right > div > div > div > div > ul > li:nth-child(1) > a'
-        util.click_element(By.CSS_SELECTOR, goToSignInEleValue)
-        
-        emailField = 'ctrl_pageLogin_login2'
-        passwordField = 'ctrl_pageLogin_password2'
-        # set field to signup
-        util.set_text(By.ID, emailField, email)
-        util.set_text(By.ID, passwordField, password)
-        
-        # click signup button
-        signInButtonSelector = '#content > div > div > div > div > form > div.TinhteMods_Form_Wrapper > input.button.primary'
-        util.click_element(By.CSS_SELECTOR, signInButtonSelector)
-
-
-        
-        
 
 class SignUpTest(unittest.TestCase):
-    signupPage = SignInPage()
+    def test_sign_in(self):
+        util.sign_in("dang.nguyen.100420@hcmut.edu.vn", "Xxt]jdoxX0410")
+
+        error_form_selector = '.TinhteMods_Form_Error > div > span'
+        error_form_message = util.driver.find_element(By.CSS_SELECTOR, error_form_selector).get_attribute('innerHTML')
+        print('test hereeee', error_form_message)
+
+        expected_err_msg = ''
+
+        assert error_form_message == ''
+
+    #XenForoUniq7 > div > span
+    #XenForoUniq9 > div > span
     
-    def testSignIn1(self):
-        self.signupPage.signIn("dang.nguyen.100420@hcmut.edu.vn", "Xxt]jdoxX0410")
         
-
-
 if __name__ == "__main__":
     unittest.main(verbosity=2)
     util.driver.quit()
--- util.py
+++ util.py
@@ -3,6 +3,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+import time
 
 
 class Util:
@@ -36,6 +37,28 @@ class Util:
     def set_text(self, byType: By, eleValue: str, text: str):
         self.driver.find_element(byType, eleValue).clear()
         self.driver.find_element(byType, eleValue).send_keys(text)
+
+    def sign_in(self, email, password):
+        self.go_to_main_page()
+        time.sleep(1)
+        menu_btn_ele_value = "//button[@type='button']"
+        self.click_element(By.XPATH, menu_btn_ele_value)
+        
+        self.driver.implicitly_wait(1)
+        
+        go_to_sign_in_ele_value = '#__next > div.root > header > div > div > div.jsx-3599443646.col-right.right > div > div > div > div > ul > li:nth-child(1) > a'
+        self.click_element(By.CSS_SELECTOR, go_to_sign_in_ele_value)
+        
+        email_field = 'ctrl_pageLogin_login2'
+        password_field = 'ctrl_pageLogin_password2'
+        # set field to signup
+        self.set_text(By.ID, email_field, email)
+        self.set_text(By.ID, password_field, password)
+        
+        # click signup button
+        sign_in_button_selector = '#content > div > div > div > div > form > div.TinhteMods_Form_Wrapper > input.button.primary'
+        self.click_element(By.CSS_SELECTOR, sign_in_button_selector)
+
         
         
         
